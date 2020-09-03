2fd4b111745ce074f865f51d3c209f1e2d5ab6e9
Citrus.T <datdaisukemikancha@gmail.com>
2020-09-03 09:47:24 +0900

    提出

--- v0903/Form1.cs
+++ v0903/Form1.cs
@@ -12,9 +12,18 @@ namespace v0903
 {
     public partial class Form1 : Form
     {
+        int vx = rand.Next(-20, 20);
+        int vy = rand.Next(-20, 20);
+        private static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + vx;
+            label1.Top = label1.Top +vy;
         }
     }
 }
